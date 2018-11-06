10536a9167b4327ecd8580afa5b26b356a245deb
Moritz <moeridoe@web.de>
2018-11-06 18:45:41 +0100

    battlefield window

--- src/field/Battlefield.java
+++ src/field/Battlefield.java
@@ -1,10 +1,16 @@
 package field;
 
-public class Battlefield {
-	private int field_width;
-	private int field_height;
+import javax.swing.JFrame;
+
+public class Battlefield extends JFrame {
+	private int field_width = 400;
+	private int field_height = 400;
 	
 	public Battlefield() {
-		
+		setSize(field_width, field_height);
+		setTitle("Shippps");
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
+import field.Battlefield;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		new Battlefield();
 	}
 }
