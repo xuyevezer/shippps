b8317b13fd92744de243c040b81604cb140c3c16
xuyevezer <33375054+xuyevezer@users.noreply.github.com>
2018-11-06 18:23:50 +0100

    initial commit

---
+++ src/field/Battlefield.java
@@ -0,0 +1,10 @@
+package field;
+
+public class Battlefield {
+	private int field_width;
+	private int field_height;
+	
+	public Battlefield() {
+		
+	}
+}
---
+++ src/objects/Ship.java
@@ -0,0 +1,60 @@
+package objects;
+
+import main.Position;
+
+public class Ship {
+	private Position pos;
+	private int size_x;
+	private int size_y;
+	private int health;
+	private int damage;
+	//private int speed;
+	
+	public Ship(Position pos, int size_x, int size_y, int health, int damage) {
+		this.pos = pos;
+		this.size_x = size_x;
+		this.size_y = size_y;
+		this.health = health;
+		this.damage = damage;
+	}
+
+	public Position getPos() {
+		return pos;
+	}
+
+	public void setPos(Position pos) {
+		this.pos = pos;
+	}
+
+	public int getSize_x() {
+		return size_x;
+	}
+
+	public void setSize_x(int size_x) {
+		this.size_x = size_x;
+	}
+
+	public int getSize_y() {
+		return size_y;
+	}
+
+	public void setSize_y(int size_y) {
+		this.size_y = size_y;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+}
