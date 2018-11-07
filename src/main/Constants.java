132392a9cab1b5ddd173754184da7dcb104234ab
Moritz <moeridoe@web.de>
2018-11-07 16:18:32 +0100

    added alternative submarine image

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -15,38 +15,38 @@ public class Constants {
 	
 	//-----------------------------SHIPS--------------------------------------------//
 	//----------------BATTLESHIP--------------//
-	public static int BATTLESHIP_SIZE_X = 209;
-	public static int BATTLESHIP_SIZE_Y = 31;
+	public static int BATTLESHIP_SIZE_X = 31;
+	public static int BATTLESHIP_SIZE_Y = 209;
 	public static int BATTLESHIP_HEALTH = 200;
 	public static int BATTLESHIP_DAMAGE = 40;
 	
 	//-----------------CARRIER---------------//
-	public static int CARRIER_SIZE_X = 189;
-	public static int CARRIER_SIZE_Y = 57;
+	public static int CARRIER_SIZE_X = 57;
+	public static int CARRIER_SIZE_Y = 189;
 	public static int CARRIER_HEALTH = 250;
 	public static int CARRIER_DAMAGE = 20;
 	
 	//-----------------CRUISER---------------//
-	public static int CRUISER_SIZE_X = 128;
-	public static int CRUISER_SIZE_Y = 23;
+	public static int CRUISER_SIZE_X = 23;
+	public static int CRUISER_SIZE_Y = 128;
 	public static int CRUISER_HEALTH = 150;
 	public static int CRUISER_DAMAGE = 25;
 	
 	//-----------------DESTROYER------------//
-	public static int DESTROYER_SIZE_X = 100;
-	public static int DESTROYER_SIZE_Y = 20;
+	public static int DESTROYER_SIZE_X = 20;
+	public static int DESTROYER_SIZE_Y = 100;
 	public static int DESTROYER_HEALTH = 100;
 	public static int DESTROYER_DAMAGE = 24;
 	
 	//-----------------PATROL------------//
-	public static int PATROL_SIZE_X = 55;
-	public static int PATROL_SIZE_Y = 13;
+	public static int PATROL_SIZE_X = 13;
+	public static int PATROL_SIZE_Y = 55;
 	public static int PATROL_HEALTH = 80;
 	public static int PATROL_DAMAGE = 13;
 	
 	//-----------------SUBMARINE------------//
-	public static int SUBMARINE_SIZE_X = 142;
-	public static int SUBMARINE_SIZE_Y = 35;
+	public static int SUBMARINE_SIZE_X = 35;
+	public static int SUBMARINE_SIZE_Y = 142;
 	public static int SUBMARINE_HEALTH = 80;
 	public static int SUBMARINE_DAMAGE = 24;
 	
--- src/objects/Submarine.java
+++ src/objects/Submarine.java
@@ -13,7 +13,7 @@ public class Submarine extends Ship {
 	public Submarine(Position pos) {
 		super(pos, Constants.SUBMARINE_SIZE_X, Constants.SUBMARINE_SIZE_Y, Constants.SUBMARINE_HEALTH, Constants.SUBMARINE_DAMAGE);
 		try {
-			image = ImageIO.read(new File("res/submarine.png"));
+			image = ImageIO.read(new File("res/submarinedark.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -22,7 +22,7 @@ public class Submarine extends Ship {
 	public Submarine(double x, double y, double orientation) {
 		super(x, y, orientation, Constants.SUBMARINE_SIZE_X, Constants.SUBMARINE_SIZE_Y, Constants.SUBMARINE_HEALTH, Constants.SUBMARINE_DAMAGE);
 		try {
-			image = ImageIO.read(new File("res/submarine.png"));
+			image = ImageIO.read(new File("res/submarinedark.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
